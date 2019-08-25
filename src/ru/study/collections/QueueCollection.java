712431e8f7ac35bf913553b786e145947374ef39
Alexei Levitsky <ydav-07@mail.ru>
2019-08-25 13:18:14 +0300

    FirstCommit

--- src/ru/study/collections/QueueCollection.java
+++ src/ru/study/collections/QueueCollection.java
@@ -1,7 +1,11 @@
 package ru.study.collections;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class QueueCollection {
     public static void main(String[] args) {
+        Queue queue = new PriorityQueue();
 
     }
 }
