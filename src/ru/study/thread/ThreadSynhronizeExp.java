b31cbcebfb854b48711687c8c7c6c8224d4105f4
LevitskyAlexei <ydav-07@mail.ru>
2019-09-25 22:36:03 +0300

    add correct in synchronized thread

--- src/ru/study/thread/ThreadSynhronizeExp.java
+++ src/ru/study/thread/ThreadSynhronizeExp.java
@@ -3,7 +3,7 @@ package ru.study.thread;
 public class ThreadSynhronizeExp {
     public static void main(String[] args) throws InterruptedException {
         Resources resources = new Resources();
-        resources.i = 5;
+        resources.setI(5);
         NewThreadExpl newThreadEx = new NewThreadExpl();
         newThreadEx.setName("one");
         NewThreadExpl newThreadEx1 = new NewThreadExpl();
@@ -14,7 +14,7 @@ public class ThreadSynhronizeExp {
         newThreadEx1.start();
         newThreadEx.join();
         newThreadEx1.join();
-        System.out.println(resources.i);
+        System.out.println(resources.getI());
     }
 }
 class NewThreadExpl extends Thread{
@@ -31,7 +31,15 @@ class NewThreadExpl extends Thread{
 }
 
 class Resources{
-     int i;
+    private int i;
+
+    public int getI() {
+        return i;
+    }
+
+    public synchronized void setI(int i) {
+        this.i = i;
+    }
 
     public synchronized void changeI(){
         int i = this.i;
